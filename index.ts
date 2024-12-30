ef25788a1ee49d1f75d4915eb47b63dd6da2b112
Chris Hatton <mail@ckhatton.com>
2024-12-30 22:35:58 +0000

    build(log): convert console log statements

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import colours from './utilities/colours';
+import log from './utilities/log';
 import yargs from 'yargs-parser';
 
 console.log(
@@ -12,32 +13,27 @@ console.log(
     '    ╚═════╝╚══════╝╚═╝╚══════╝  Command Line Interface Example\n' +
     ' ===============================================================\n'
 );
-console.log('' + '   --server, -s to run a Bun server.\n\n');
+log('--server, -s to run a Bun server.\n\n');
 
 // Yargs Data
-console.log(colours.cyan, '   ___ 🛂 Yargs Data ___\n');
+log('___ 🛂 Yargs Data ___\n', colours.cyan);
 const rawArgs: string[] = process.argv.slice(2);
 const yargsArgs: yargs.Arguments = yargs(rawArgs) || {};
-console.log('   The raw arguments:', rawArgs);
-console.log('   The Yargs arguments:', JSON.stringify(yargsArgs));
-console.log('');
+log(`The raw arguments: ${rawArgs}`);
+log(`The Yargs arguments: ${JSON.stringify(yargsArgs)}`);
+log('');
 
 // Bun Server
 if (yargsArgs.server || yargsArgs.s) {
-  console.log(colours.cyan, '   ___ 📦 Bun Server ___\n');
+  log('___ 📦 Bun Server ___\n', colours.cyan);
   const server = Bun.serve({
     port: 3000,
     fetch(_req) {
       return new Response('Hello world! 👋 You are running on Bun! 🍞');
     },
   });
-  console.log(
-    colours.green,
-    `   Listening on http://localhost:${server.port}\n`
-  );
-  console.log(
-    '   The server will close in 60 seconds, otherwise press Ctrl+C to quit.'
-  );
+  log(`Listening on http://localhost:${server.port}\n`, colours.green);
+  log('The server will close in 60 seconds, otherwise press Ctrl+C to quit.');
   setTimeout(() => {
     process.exit(0);
   }, 60000);
