d59baf6f8158fcebeadde2afad5352347043695f
Chris Hatton <mail@ckhatton.com>
2024-12-31 00:49:25 +0000

    build(github): task

--- index.ts
+++ index.ts
@@ -1,16 +1,20 @@
 import loadArgv from './tasks/loadAgrv';
 import loadBunServer from './tasks/loadBunServer.ts';
 import loadEnvFile from './tasks/loadEnvFile';
+import loadGitHub from './tasks/loadGitHub';
 import loadHeader from './tasks/loadHeader';
 
 // Header
 loadHeader();
 
 // Environment Variables
-const { greeting, port } = loadEnvFile();
+const { githubToken, greeting, port } = loadEnvFile();
 
 // Bun.argv Data
 const parsedArgs = loadArgv();
+
+// GitHub
+await loadGitHub(githubToken);
 
 // Bun Server
 if (parsedArgs.server || parsedArgs.s) {
--- tasks/loadEnvFile.ts
+++ tasks/loadEnvFile.ts
@@ -2,6 +2,7 @@ import colours from '../utilities/colours';
 import log from '../utilities/log';
 
 function loadEnvFile(): {
+  githubToken: string;
   greeting: string;
   port: number;
 } {
@@ -16,12 +17,23 @@ function loadEnvFile(): {
   })();
   const greeting: string =
     Bun.env.GREETING || `Hello world! 👋 You are running on Bun! 🍞`;
+  const githubToken: string = Bun.env.GITHUB_PERSONAL_ACCESS_TOKEN || '';
 
   log(`PORT: ${port}`);
   log(`GREETING: ${greeting}`);
+  log(
+    `GITHUB_PERSONAL_ACCESS_TOKEN: ${(() => {
+      if (githubToken.length > 32) {
+        return githubToken.substring(0, 32) + '...';
+      } else {
+        return githubToken ? githubToken : '-';
+      }
+    })()}`,
+    githubToken ? undefined : colours.red
+  );
   log('');
 
-  return { greeting, port };
+  return { githubToken, greeting, port };
 }
 
 export default loadEnvFile;
